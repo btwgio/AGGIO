2e398114c8f359c7e6493288ef88377e578ed49d
Nathan Cavalcante <105390203+N4teC@users.noreply.github.com>
2024-09-18 21:06:54 +0000

    agiota correção

--- models/agiota.py
+++ models/agiota.py
@@ -11,7 +11,7 @@ class Agiota(Usuario):
     def set_credito(self, credito: float):
         self.credito = credito
     def __str__(self):
-        return f" {super().get_id()} - {super().get_nome()} - {super().get_celular()} - {super().get_endereco()} - {self.__credito} R$"
+        return f" {super().get_id()} - {super().get_nome()} - {super().get_celular()} - {super().get_endereco()} - {self.credito} R$"
 
 class Agiotas(Modelo):
     @classmethod
