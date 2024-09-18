929e12ad46eb210b09dc94188cfbdfaf1d063985
Giovanna <130415868+btwgio@users.noreply.github.com>
2024-09-17 22:29:28 -0300

    UI

--- UI.py
+++ UI.py
@@ -1,10 +1,10 @@
-from view import View
+from views import View
 
 class UIa:
     @staticmethod
     def menu():
-        print("Clientes")
-        print("1-Inserir, 2-Listar, 3-Atualizar, 4-Excluir, 9-Fim")
+        print("Olá! Tudo bem? Insira a opção mais adequada para ação que deseja realizar.")
+        print("1-Inserir empréstimo\n2-Listar empréstimos\n3-Atualizar\n4-Excluir empréstimo\n9-Fim")
         return int(input("Escolha uma opção: "))
     @staticmethod
     def main():
@@ -15,16 +15,14 @@ class UIa:
             if op == 2: UIa.cliente_listar()
             if op == 3: UIa.cliente_atualizar()
             if op == 4: UIa.cliente_excluir()
-            
+            if op == 9: UIa.cliente_fim()
+            #inserir exceção de ERRO
     @staticmethod
     def cliente_inserir():
-        #id = int(input("Informe o id: "))
         nome = input("Informe o nome: ")
         celular = input("Informe o celular: ")
         endereco = input("Informe o endereço: ")
-        #saldo = input("Informe o saldo: ")
         View.cliente_inserir(nome, celular, endereco)
-
     @staticmethod
     def cliente_listar():
         for cliente in View.cliente_listar(): 
@@ -32,18 +30,16 @@ class UIa:
     @staticmethod
     def cliente_atualizar():
         UIa.cliente_listar()
-        id = int(input("Informe o id do cliente a ser atualizado: "))
+        #id = int(input("Informe o id do cliente a ser atualizado: "))
         nome = input("Informe o novo nome: ")
         celular = input("Informe o novo celular: ")
         endereco = input("Informe o novo endereco: ")
         View.cliente_atualizar(id, nome, celular, endereco)
-
     @staticmethod
     def cliente_excluir():
         UIa.cliente_listar()
         id = int(input("Informe o id do cliente a ser excluído: "))
-        View.cliente_excluir(id)
-    
+        View.cliente_excluir(id)  
     #COBRADOR
     @staticmethod
     def cobrador_inserir():
@@ -52,8 +48,7 @@ class UIa:
         celular = input("Informe o celular: ")
         endereco = input("Informe o endereço: ")
         placa = input("Informe o número da placa do veículo: ")
-        View.inserir_cobrador(nome, celular, endereco, placa)
-    
+        View.inserir_cobrador(nome, celular, endereco, placa)  
     @staticmethod
     def cobrador_listar():
         for cobrador in View.listar_cobrador():
